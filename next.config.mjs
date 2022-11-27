171079cea0e26a2bd115cad1084e14d1ff0b880b
nanimono_demonai <masa9nao5.n4.18@gmail.com>
2022-11-28 00:41:19 +0900

    superjsonできるようにしてts-nodeの設定をする

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,35 @@
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import {withSuperjson} from 'next-superjson';
 
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  experimental: { esmExternals: true },
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.mdx/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: "@mdx-js/loader",
-          options: {
-            /** @type {import('@mdx-js/loader').Options} */
-            remarkPlugins: [
-              remarkFrontmatter,
-              [remarkMdxFrontmatter, { name: "frontmatter" }],
+    experimental: {esmExternals: true},
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.mdx/,
+            use: [
+                options.defaultLoaders.babel,
+                {
+                    loader: "@mdx-js/loader",
+                    options: {
+                        /** @type {import('@mdx-js/loader').Options} */
+                        remarkPlugins: [
+                            remarkFrontmatter,
+                            [remarkMdxFrontmatter, {name: "frontmatter"}],
+                        ],
+                    },
+                },
             ],
-          },
-        },
-      ],
-    });
-    config.module.rules = config.module.rules.map((e) => ({ ...e, resourceQuery: { not: [/raw/] } }));
-    config.module.rules.push({
-      resourceQuery: /raw/,
-      type: "asset/source",
-    });
-    return config;
-  },
+        });
+        config.module.rules = config.module.rules.map((e) => ({...e, resourceQuery: {not: [/raw/]}}));
+        config.module.rules.push({
+            resourceQuery: /raw/,
+            type: "asset/source",
+        });
+        return config;
+    },
 };
-export default nextConfig;
+export default withSuperjson()(nextConfig);
