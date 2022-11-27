930d1217f7feb12652a58261f2018c3bb6799e1a
nanimono_demonai <masa9nao5.n4.18@gmail.com>
2022-11-28 00:59:46 +0900

    記事追加する

--- scripts/addEntriesToDB.ts
+++ scripts/addEntriesToDB.ts
@@ -1,5 +1,33 @@
+import { addEntry } from "src/service/entries/addEntry";
 import { getEntries } from "src/service/fs/getEntries";
+import { getHistory, showHistory } from "src/service/git/git";
+import { frontMatterParser } from "src/utils/parsers/FrontMatterParser";
 
 const addEntriesToDB = async () => {
-  const entries = await getEntries("entries");
+  const entriesFilename = await getEntries("entries");
+  await Promise.all(
+    entriesFilename.map(async ({ name, path }) => {
+      const history = await getHistory(path);
+      history.map(async (e) => {
+        const { date, hash } = e;
+        try {
+          const source = await showHistory({ hash, file: path });
+          const { frontMatter } = frontMatterParser(source);
+          const entry = {
+            createdAt: date,
+            pageTitle: frontMatter.title,
+            pid: name,
+            revision: hash,
+            source,
+            tags: frontMatter.tags,
+          };
+          await addEntry(entry);
+        } catch (e) {
+          console.log(e);
+        }
+      });
+    })
+  );
 };
+
+addEntriesToDB();
