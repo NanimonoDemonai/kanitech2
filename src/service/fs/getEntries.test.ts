42f9d826b30d43c3ae640737efbddc4a5a74b171
nanimono_demonai <masa9nao5.n4.18@gmail.com>
2022-11-28 00:44:30 +0900

    getEntriesはpathを渡すようにする

--- src/service/fs/getEntries.test.ts
+++ src/service/fs/getEntries.test.ts
@@ -7,7 +7,10 @@ describe("getEntries", () => {
   it("getEntries", async () => {
     const entries = await getEntries("test_resource");
     expect(entries).toIncludeAllMembers([
-      "test_resource/how_to_mdx_bundling.mdx",
+      {
+        name: "how_to_mdx_bundling",
+        path: "test_resource/how_to_mdx_bundling.mdx",
+      },
     ]);
   });
 });
--- src/service/fs/getEntries.ts
+++ src/service/fs/getEntries.ts
@@ -1,4 +1,10 @@
+import path from "path";
 import fg from "fast-glob";
 
-export const getEntries = async (path: string): Promise<string[]> =>
-  await fg([`${path}/**.mdx`]);
+export const getEntries = async (
+  rootPath: string
+): Promise<{ name: string; path: string }[]> =>
+  (await fg([`${rootPath}/**.mdx`], { objectMode: true })).map((e) => ({
+    name: path.parse(e.name).name,
+    path: e.path,
+  }));
