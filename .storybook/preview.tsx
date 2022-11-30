82f41688bf98e609c2e271cdf3077957b3695a67
nanimono_demonai <masa9nao5.n4.18@gmail.com>
2022-11-30 23:53:54 +0900

    storybook導入

---
+++ .storybook/preview.tsx
@@ -0,0 +1,14 @@
+import "../styles/global.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+export const globalTypes = {
+  darkMode: true,
+};
