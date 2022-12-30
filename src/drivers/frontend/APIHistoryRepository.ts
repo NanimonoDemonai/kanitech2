c81178badc28c927540a8c44747d4686496a5975
nanimono_demonai <masa9nao5.n4.18@gmail.com>
2022-12-31 00:47:33 +0900

    EntryHistoryRepositoryは分ける

--- src/drivers/frontend/APIHistoryRepository.ts
+++ src/drivers/frontend/APIHistoryRepository.ts
@@ -1,12 +1,8 @@
-import { EntryWithHistory, SourceHistory } from "src/domains/Entry";
+import { SourceHistory } from "src/domains/Entry";
 import { trpcClient } from "src/infrastructures/trpc/trpc";
 import { EntryHistoryRepositoryInterface } from "src/useCases/EntryHistoryUseCases";
 
 export class APIHistoryRepository implements EntryHistoryRepositoryInterface {
-  findAll(): Promise<EntryWithHistory[]> {
-    return Promise.resolve([]);
-  }
-
   findHistoryByPid(pid: string): Promise<SourceHistory[]> {
     return trpcClient.historyByPid.query({ pid });
   }
